1ddcc529f5ba562b4a1cb2d1af26424bbf898f77
Attila Gombos <attila.gombos@effective-range.com>
2026-01-16 13:39:06 +0100

    Add CI for packaging

--- check_drafts.sh
+++ check_drafts.sh
@@ -3,10 +3,6 @@
 set -e
 
 python3 - <<EOF
-import os
-print("env:", {k: os.environ.get(k) for k in (
-    "ZMQ_BUILD_DRAFT","ZMQ_DRAFT_API","ZMQ_BUNDLED","PIP_NO_BINARY"
-)})
 import zmq
 assert zmq.has("draft"), "Draft sockets NOT enabled"
 print("Draft sockets enabled")
